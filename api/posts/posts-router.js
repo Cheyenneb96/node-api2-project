800b940c31a6a2f71b196d312be64993c91f68a1
Cheyenne Bowman <cheyennebowman1212123@gmail.com>
2021-11-30 16:12:20 -0600

    all test

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -84,8 +84,8 @@ router.put('/:id',(req, res)=>{
         })
     } else{
         Post.findById(req.params.id)
-        .then( stuff =>{
-            if (!stuff){
+        .then( info =>{
+            if (!info){
                 res.status(404).json({
                     message: "The post with the spicified ID does not exist",
                 })
@@ -113,8 +113,25 @@ router.put('/:id',(req, res)=>{
     }
 })
 
-router.get('/:id/messages',(req, res)=>{
+router.get('/:id/comments', async (req, res)=>{
+try{ 
+    const post =await Post.findById(req.params.id)
+    if (!post){
+        res.status(404).json({
+            message: " the post with the spicified id does not exist",
+        })
+    } else{
+        const comments = await Post.findPostComments(req.params.id)
+        res.json(comments)
+    }
 
+}catch (err) {
+    res.status(500).json({
+        message: " The post could not be removed",
+        err: err.message,
+        stack: err.stack,
+     })
+}
 })
 
 module.exports = router
